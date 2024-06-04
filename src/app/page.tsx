0b9b256b20827267296f0f4ff9e05326fe248db2
scott susanto <susantoscott@gmail.com>
2024-06-04 09:36:13 -0700

    NEXT_PUBLIC_

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ export default async function Home() {
     <>
       <h1>Hello there 👋</h1>
       <ul>
-        <li>Env 1: {process.env.ENV_1}</li>
-        <li>Env 2: {process.env.ENV_2}</li>
-        <li>Env 3: {process.env.ENV_3}</li>
+        <li>Env 1: {process.env.NEXT_PUBLIC_ENV_1}</li>
+        <li>Env 2: {process.env.NEXT_PUBLIC_ENV_2}</li>
+        <li>Env 3: {process.env.NEXT_PUBLIC_ENV_3}</li>
       </ul>
     </>
   );
